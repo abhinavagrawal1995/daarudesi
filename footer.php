5d4cf946b7e8f0a7e0181b7b5004ef2ba6e8b45a
abhinavagrawal1995 <abhinavagrawal1995@gmail.com>
2016-04-16 01:57:05 +0530

    auto push

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
-<footer id="footer">
-						<div class="inner">
+<footer id="footer" style="margin-top:0;padding: 1em 0 3em 0; height: 100px; background: none;">
+						<div class="inner" style=" margin-top: 5%;;padding-top: 0;">
 							<!--  -->
-							<ul class="copyright">
+							<ul class="copyright" style="margin-top:0;padding: 0;">
 							<li>Developer: <a href="http://abhinavagrawal.in">Abhinav Agrawal</a></li>
 							</ul>
 						</div>
